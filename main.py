a247ee23852a340a51db123985eda7d93db53ac6
Parsa <parsadaj@ut.ac.ir>
2019-07-27 08:36:27 +0430

    almost done! basic configs added! one more thing to do is to determine the winner and end the game at correct time.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Tic Tac Toe Game!')
 
+clock = pygame.time.Clock()
+
 colorWhite = (255, 255, 255)
 colorBlack = (0, 0, 0)
 
@@ -19,4 +21,38 @@ status = [[0, 0, 0],
           [0, 0, 0],
           [0, 0, 0]]
 
+game_ended = False
+game_aborted = False
+turn = 'X'
+played_turns = 0
+while not game_aborted:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            game_aborted = True
+        if event.type == pygame.MOUSEBUTTONDOWN and not game_ended:
+            pos = event.pos
+
+            row = int(pos[0]/200)
+            col = int(pos[1]/200)
+            if status[row][col] == 0:
+                status[row][col] = turn
+                turn = 'X' if turn == 'O' else 'O'
+                played_turns += 1
+    screen.fill(colorWhite)
+    for i in range(1,3):
+        screen.blit(board_columns, (i*200, 0))
+        screen.blit(board_rows, (0, i*200))
+    for row in range(3):
+        for col in range(3):
+            pic = X if status[row][col] == 'X' else \
+                  O if status[row][col] == 'O' else 0
+            if pic:
+                screen.blit(pic, (row*200, col*200))
+
+    if played_turns == 9:
+        game_ended = True
+
+    pygame.display.update()
+    clock.tick(60)
 pass
